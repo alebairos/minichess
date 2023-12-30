57b5a8db7caf6a14a40629cb3b83c16ebabefa16
Alexandre Bairos <alexandre.bairos@gmail.com>
2023-12-30 14:26:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+import Minichess from './components/Minichess';
+function App() {
+  return (
+  <div id="app" >
+    <Minichess />
+  </div>);
+}
+export default App;
---
+++ src/components/Tree.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './Tree.css';
+export default function tree() {
+    return <div>tree!!!!!</div>
+}
